9eacd3ac7f56c59c916f075c9237fb571b8476f4
Zhen You <youzhen93@gmail.com>
2025-11-18 15:35:12 +0800

    Fix assignment operators and improve readability

--- pheno-geno/Cox_proportional_regression_model_ESKD_genotypes.R
+++ pheno-geno/Cox_proportional_regression_model_ESKD_genotypes.R
@@ -11,7 +11,7 @@ library(dplyr)
 library(cowplot)
 
 # load data 
-eskd = read.table("./test_data/ESKD_data.xls", header = T, sep = "\t")[,1:3]
+eskd <- read.table("./test_data/ESKD_data.xls", header = T, sep = "\t")[,1:3]
 # columns: time status genotype
 # status: 1 > normal; 2 > ESKD
 
@@ -22,8 +22,8 @@ eskd %>% group_by(genotype) %>% mutate(freq = n()) %>%
 
 
 # Cox proportional hazards regression models
-cox1 = coxph(Surv(time, status) ~ 1 + genotype, data = eskd)
-cox1 = summary(cox1)
+cox1 <- coxph(Surv(time, status) ~ 1 + genotype, data = eskd)
+cox1 <- summary(cox1)
 print(cox1_sum$sctest[3]) # log-rank p
 # P = 1.599728e-08
 print(cox1_sum$coefficients[,1]) # HR
@@ -35,7 +35,7 @@ genotypeCOL4A5_collagen_hemi  genotypeCOL4A5_collagen_het      genotypeCOL4A5_NC
       genotypeCOL4A5_NC1_het 
                   1.02955536 
 """
-cox1_newdata = data.frame(genotype = cox1$xlevels, time = rep(mean(eskd$time), 2))
+cox1_newdata <- data.frame(genotype = cox1$xlevels, time = rep(mean(eskd$time), 2))
 rownames(cox1_newdata) = cox1_newdata$genotype
 cox1_fit <- survfit(cox1, newdata = cox1_newdata)
 
